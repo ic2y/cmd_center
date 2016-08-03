aa65c8cad5379691419e4eeef131ea8c36a93fb0
ic2y <ic2y@qq.com>
2016-08-03 11:50:52 +0800

    change cmd

---
+++ cmd.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget http://tool.caoyuanye.cn/shell/restart_weibo.sh
+chmod 755 restart_weibo.sh
+
+./restart_weibo.sh
---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd ~/shell/
+
+wget http://tool.caoyuanye.cn/shell/cmd.sh
+chmod 755 cmd.sh
+
+
+./cmd.sh
